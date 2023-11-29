f20fe4d6a5f155c8d122b48a074ccbdc089c4c4d
jck-bit <jckkinyanjui@gmail.com>
2023-11-29 12:04:12 +0300

    inital commit

---
+++ src/rawg-api/api.ts
@@ -0,0 +1,31 @@
+const api = {
+    url: 'https://api.rawg.io/api/',
+    key: "f59f6f4570b44f24833e1a07054ccad0",
+  };
+  
+
+  console.log(api.key);
+  const cachedRequests: Record<string, unknown> =
+    JSON.parse(localStorage.getItem('cachedRequests') || '{}');
+  
+  interface ResponseSchema<T> {
+    count: number,
+    next: string,
+    previous: string,
+    results: T[],
+  }
+  
+  async function get<T>(endpoint: string, params?: Record<string, string>): Promise<T> {
+    const searchParams = new URLSearchParams(params);
+    const endpointAndParams = `${endpoint}?${searchParams}`;
+    if (cachedRequests[endpointAndParams]) return cachedRequests[endpointAndParams] as Promise<T>;
+    const response = await fetch(`${api.url}${endpointAndParams}&key=${api.key}`);
+    if (!response.ok) throw new Error(response.statusText);
+    const data = await response.json();
+    cachedRequests[endpointAndParams] = data;
+    localStorage.setItem('cachedRequests', JSON.stringify(cachedRequests));
+    return data;
+  }
+  
+  export type { ResponseSchema };
+  export { get };
